f9d595696430f28dd7818ed6765afdf457244a91
emery <215300895+xexemery@users.noreply.github.com>
2025-07-31 12:08:47 +0100

    Add player character

---
+++ scripts/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+const SPEED = 60.0
+const JUMP_VELOCITY = -200.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction := Input.get_axis("left", "right")
+
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	if is_on_floor():
+		if Input.is_action_pressed("down"):
+			direction = 0
+			animated_sprite.play("crouch")
+		elif direction != 0:
+			animated_sprite.play("run") 
+		else:
+			animated_sprite.play("idle")
+	else:
+		animated_sprite.play("jump")
+
+	# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
