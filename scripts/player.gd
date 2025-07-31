ab346ce46cb892d2973e52ac42bcd1716714ef62
emery <215300895+xexemery@users.noreply.github.com>
2025-07-31 13:22:21 +0100

    Improve player logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 const SPEED = 60.0
 const JUMP_VELOCITY = -200.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var direction: float
 
 
 func _physics_process(delta: float) -> void:
@@ -17,7 +19,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
-	var direction := Input.get_axis("left", "right")
+	direction = Input.get_axis("left", "right")
 
 	# Flip the sprite
 	if direction > 0:
@@ -29,10 +31,10 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_pressed("down"):
 			direction = 0
 			animated_sprite.play("crouch")
-		elif direction != 0:
-			animated_sprite.play("run") 
-		else:
+		elif direction == 0:
 			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
 
