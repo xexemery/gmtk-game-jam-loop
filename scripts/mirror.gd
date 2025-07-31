88a5a700784552e62daba56c2ea1e21cd130e274
emery <215300895+xexemery@users.noreply.github.com>
2025-07-31 18:24:31 +0100

    Reorganise files

--- scripts/mirror.gd
+++ scripts/mirror.gd
@@ -4,10 +4,17 @@ extends StaticBody2D
 
 @onready var area: Area2D = $Area2D
 @onready var label: Label = $Label
+@onready var text_box: CanvasLayer = %TextBox
 
 
 func interact() -> void:
-	print("mirror")
+	text_box.queue_text("Why the long face?")
+	text_box.queue_text("People keep changing...")
+	text_box.queue_text("Is that such a bad thing?")
+	text_box.queue_text("I mean...")
+	text_box.queue_text("What, you wanna stay here?")
+	text_box.queue_text("These people were having fun...")
+	text_box.queue_text("These people... are stuck in the past")
 
 
 func _on_area_entered(body: Node2D) -> void:
