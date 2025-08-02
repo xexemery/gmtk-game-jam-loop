03c7e886b143cca8a3061280b470c3164ab40574
emery <215300895+xexemery@users.noreply.github.com>
2025-08-02 17:49:12 +0100

    Add more levels and coyote time

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -3,11 +3,13 @@ extends Character
 
 
 @onready var ray_cast: RayCast2D = $RayCast2D
+@onready var coyote_timer: Timer = $CoyoteTimer
 
 const SPEED = 60.0
 const JUMP_VELOCITY = -230.0
 
 var direction: float
+var coyote_time_activated: bool = false
 
 
 func _physics_process(delta: float) -> void:
@@ -16,8 +18,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or not coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
+		coyote_timer.stop()
+		coyote_time_activated = true
 
 	# Get the input direction: -1, 0, 1
 	direction = Input.get_axis("left", "right")
@@ -30,6 +34,7 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 		ray_cast.target_position = Vector2(-8, 0)
 
+	# Play animations
 	if is_on_floor():
 		if Input.is_action_pressed("down"):
 			direction = 0
@@ -38,8 +43,16 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
+
+		if coyote_time_activated:
+			coyote_time_activated = false
+			coyote_timer.stop()
 	else:
 		animated_sprite.play("jump")
+
+		if not coyote_time_activated:
+			coyote_timer.start()
+			coyote_time_activated = true
 
 	# Apply movement
 	if direction:
